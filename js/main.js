329821b027ffafcbb12411cf22c0b0a535714c9b
chaidul <technicalgyanassam@gmail.com>
2022-02-04 15:40:48 +0530

    added navbar on the top

--- js/main.js
+++ js/main.js
@@ -36,3 +36,16 @@ function ImageTobase64(file) {
     };
   }
 }
+
+/*Navbar script*/
+(function () {
+  var navButton = document.querySelector("#nav-menu-button");
+  var navUl = document.querySelector(".nav-ul");
+
+  function toggleMobileMenu() {
+    navUl.classList.toggle("hide-ul");
+  }
+  navButton.onclick = toggleMobileMenu;
+}());
+
+/*End Navbar script*/
